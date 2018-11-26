a374858de8d0d91017d844fef3add00d616bdd11
ddolsoon <minseong6284@naver.com>
2018-11-26 10:01:08 +0900

    [#4] ElasticSearch 설정파일 추가(RestHighLevelClient)

---
+++ src/main/java/com/naver/mapsearch/mapsearchservice/config/ElasticSearchConfiguration.java
@@ -0,0 +1,62 @@
+package com.naver.mapsearch.mapsearchservice.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.AbstractFactoryBean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticSearchConfiguration extends AbstractFactoryBean<RestHighLevelClient> {
+
+    private static final Logger logger = LoggerFactory.getLogger(ElasticSearchConfiguration.class);
+
+    private static final String HOST_IP = "106.10.33.83";
+    private static final Integer PORT = 9200;
+
+    @Value("${spring.data.elasticsearch.cluster-nodes}")
+    private String clusterNodes;
+    @Value("${spring.data.elasticsearch.cluster-name}")
+    private String clusterName;
+    private RestHighLevelClient restHighLevelClient;
+
+    @Override
+    public void destroy() {
+        try {
+            if (restHighLevelClient != null) {
+                restHighLevelClient.close();
+            }
+        } catch (final Exception e) {
+            logger.error("Error closing ElasticSearch client: ", e);
+        }
+    }
+
+    @Override
+    public Class<RestHighLevelClient> getObjectType() {
+        return RestHighLevelClient.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
+
+    @Override
+    public RestHighLevelClient createInstance() {
+        return buildClient();
+    }
+
+    private RestHighLevelClient buildClient() {
+        try {
+            restHighLevelClient = new RestHighLevelClient(
+                    RestClient.builder(
+                            new HttpHost(HOST_IP, PORT, "http")));
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return restHighLevelClient;
+    }
+}
