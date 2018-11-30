85660806f5953a4b6b5d4839f23f9a3bd8570f04
ddolsoon <minseong6284@naver.com>
2018-11-30 11:45:00 +0900

    [#4] 로그 포멧 설계

--- src/main/java/com/naver/mapsearch/mapsearchservice/repository/MapSearchLogRepository.java
+++ src/main/java/com/naver/mapsearch/mapsearchservice/repository/MapSearchLogRepository.java
@@ -1,30 +1,15 @@
 package com.naver.mapsearch.mapsearchservice.repository;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.naver.mapsearch.mapsearchservice.domain.MapSearchLog;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Slf4j
 @Repository
 public class MapSearchLogRepository {
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
     public void saveSearchLog(String ip, String keyword, Double latitude, Double longitude)  {
 
-        MapSearchLog mapSearchLog = new MapSearchLog(ip, keyword, latitude, longitude);
-
-        String jsonMapSearchLog = null;
-        try {
-            jsonMapSearchLog = objectMapper.writeValueAsString(mapSearchLog);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        log.info("MapSearchLogJSON : {}", jsonMapSearchLog);
+        String SearchLogFormat = String.format("ip:%s,keyword:%s,latitude:%f,longitude:%f",ip,keyword,latitude,longitude);
+        log.info("MapSearchLogJSON : {}", SearchLogFormat);
     }
 }
